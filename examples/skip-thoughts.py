1ee692054ba9a6e5a9b2a20c987afa71b3318181
anya-bel <39431144+anya-bel@users.noreply.github.com>
2019-02-01 12:58:52 +0700

    Update skip-thoughts.py

--- examples/skip-thoughts.py
+++ examples/skip-thoughts.py
@@ -4,7 +4,6 @@ import io
 import numpy as np
 import logging
 import sys
-sys.setdefaultencoding('utf8')
 import skip_thought.tools as tools
 
 
@@ -14,23 +13,18 @@ PATH_TO_DATA = '../data/senteval_data/'
 PATH_TO_SKIPTHOUGHT = 'skip-thoughts-files'
 
 
-# import skipthought and Senteval
-sys.path.insert(0, PATH_TO_SKIPTHOUGHT)
-import skipthoughts
+# import Senteval
 sys.path.insert(0, PATH_TO_SENTEVAL)
 import senteval
 
 
 def prepare(params, samples):
-    if isinstance(samples, list):
-        return params, samples
-    else:
-        raise TypeError('Samples should be list of str e.g. ["Хорошая погода.", "Интересный фильм."]')
     return
 
 def batcher(params, batch):
-    batch = [str(' '.join(sent), errors="ignore") if sent != [] else '.' for sent in batch]
-    embeddings = skipthoughts.encode(params['encoder'], batch,
+    if any(isinstance(lst, list) for lst in batch):
+        batch = [str(" ".join(sent)) for sent in batch]
+    embeddings = model.encode(params['encoder'], batch,
                                      verbose=False, use_eos=True)
     return embeddings
 
@@ -44,7 +38,7 @@ logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
 
 if __name__ == "__main__":
     model = tools.load_model('skip-thoughts-files/wiki_model.dat.npz', 'skip-thoughts-files/wiki_dict.dat', 'skip-thoughts-files/all.norm-sz100-w10-cb0-it1-min100.w2v')
-    params_senteval['encoder'] = skipthoughts.load_model()
+    params_senteval['encoder'] = model
     se = senteval.engine.SE(params_senteval, batcher, prepare)
     transfer_tasks = 'SST2'
     results = se.eval(transfer_tasks)
